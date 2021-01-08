1ac3778d05e3277c2a4a9fd4c84b5f3c6132cafa
sheesoo21 <sheesoo21@gmail.com>
2021-01-08 18:10:42 +0900

    commit

--- src/models/m_menus.js
+++ src/models/m_menus.js
@@ -14,11 +14,24 @@ module.exports = {
     insertmenu : async function(user){
         if (!user) return;
 
+        
+
         const connection = await connect();
         if(connection.error) return;
 
         try{
             const query = 'insert into menus values (?,?,?,?)';
+            //칼럼명 (menuid,storeid,name,price,menu_img);
+            /*const user = {
+                custid: "",
+                storeid: "",
+                name: "",
+                price: "",
+                menu_img: "",
+            }*/
+            //리뷰에서 필요한 데이터
+            //받을땐: 유저아이디(인덱스), 제목, 내용
+            //클라이언트에 보내줄땐: 유저아이디(인덱스), 유저닉네임, 제목, 내용
             const data = await connection.query(
                 query,[
                 user.storeid,
