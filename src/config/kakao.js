b18b3e61671a893d162718cd549cf4253822ac6b
Buning Bread <ece5074@gmail.com>
2021-01-12 17:00:22 +0900

    test

--- src/config/kakao.js
+++ src/config/kakao.js
@@ -3,5 +3,4 @@ module.exports = {
     clientID: '06f2524a997acadaab9a50ac99b93c6a',
     clientSecret: '',
     callbackURL: 'http://millplanning.ml/api/auth/oauth/kakao',
-    passReqToCallback : true,
 }
