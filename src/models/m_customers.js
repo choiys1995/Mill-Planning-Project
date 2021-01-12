1a7d1088b79f27f74acb1b9bace09c0e573147bb
sheesoo21 <sheesoo21@gmail.com>
2021-01-12 10:51:03 +0900

    modified

--- src/models/m_customers.js
+++ src/models/m_customers.js
@@ -53,9 +53,9 @@ module.exports = {
         if (connection.error) return;
 
         try {
-            const query = 'insert into customers(email, password, tel, nickname) values (?, ?, ?, ?)';
+            const query = 'insert into customers(email, password, tel, nickname, token) values (?, ?, ?, ?, ?)';
 
-            const data = await connection.query(query, [user.email, user.password, user.tel, user.nickname]);
+            const data = await connection.query(query, [user.email, user.password, user.tel, user.nickname,user.token]);
             return data;
         } catch (error) {
             return error;
